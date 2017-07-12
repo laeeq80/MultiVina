bf65cbda353ff199fe5f5a25670112364f8f2702
laeeq80 <laeeq@kth.se>
2017-07-12 17:54:21 +0200

    single file created in pdbqt and converted it to sdf using openbabel

--- multivina.sh
+++ multivina.sh
@@ -13,6 +13,11 @@ vina_split --input $1 --ligand ligand &> /dev/null
 echo "Docking ligands using vina" 
 for i in ligand*.pdbqt; do vina --ligand $i --config conf.txt; done &> /dev/null
 
+echo "Making single pdbqt file"
+for i in ligand*_out.pdbqt; do cat $i; done > single.pdbqt 
+
+echo "Converting single pdbqt file to sdf file using openbabel"
+obabel -i pdbqt single.pdbqt -o sdf -O single.sdf
 
 #echo $1
 #echo $2
