0f66c22736838f7d4210688c94ae9a8cbdded252
laeeq80 <laeeq@kth.se>
2017-07-13 19:09:08 +0200

    printing to stdout

--- multivina.sh
+++ multivina.sh
@@ -7,17 +7,20 @@ fi
 INPUT=$1
 #OUTPUT=$2
 
-echo "Splitting ligands using vina_split"
+#echo "Splitting ligands using vina_split"
 vina_split --input $1 --ligand ligand &> /dev/null
 
-echo "Docking ligands using vina" 
+#echo "Docking ligands using vina" 
 for i in ligand*.pdbqt; do vina --ligand $i --config conf.txt; done &> /dev/null
 
-echo "Making single pdbqt file"
+#echo "Making single pdbqt file"
 for i in ligand*_out.pdbqt; do cat $i; done > single.pdbqt 
 
-echo "Converting single pdbqt file to sdf file using openbabel"
-obabel -i pdbqt single.pdbqt -o sdf -O single.sdf
+#echo "Converting single pdbqt file to sdf file using openbabel"
+obabel -i pdbqt single.pdbqt -o sdf -O single.sdf &> /dev/null
+
+cat single.sdf
+rm ligand* single*
 
 #echo $1
 #echo $2
